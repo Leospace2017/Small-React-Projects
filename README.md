c2ec5953d9feab68a2b5d6089663b434b3c8767d
leospace2017 <speicher5555@gmail.com>
2023-06-15 14:07:18 +0200

    Add: Link && AudioPathTry

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Small-React-Projects DrumKit To-Do-Note TicTacToc
+
+
+[https://leospace2017.github.io/Small-React-Projects/]
--- src/component/drumkit/components/drumHelper.jsx
+++ src/component/drumkit/components/drumHelper.jsx
@@ -1,4 +1,7 @@
 
+const publicURL = process.env.PUBLIC_URL;
+const audioFilePath = `${publicURL}/component/drumkit/sounds/`;
+
 
 function buttonanimation(currentkey) {
     let activebutton = document.querySelector("." + currentkey);
@@ -11,18 +14,17 @@ function buttonanimation(currentkey) {
     }catch(e){
         console.log(e);
     }
-
 }
 
 function makeSound(key){
     const drumSound = {
-        'j' : new Audio('src/component/drumkit/sounds/kick-bass.mp3'),
-        'k' : new Audio("src/component/drumkit/sounds/crash.mp3"),
-        'l' : new Audio("src/component/drumkit/sounds/snare.mp3"),
-        'a' : new Audio("src/component/drumkit/sounds/tom-1.mp3"),
-        's' : new Audio("src/component/drumkit/sounds/tom-2.mp3"),
-        'd' : new Audio("src/component/drumkit/sounds/tom-3.mp3"),
-        'f' : new Audio("src/component/drumkit/sounds/tom-4.mp3")
+        'j' : new Audio(`${audio}kick-bass.mp3`),
+        'k' : new Audio(`${audio}crash.mp3`),
+        'l' : new Audio(`${audio}snare.mp3`),
+        'a' : new Audio(`${audio}tom-1.mp3`),
+        's' : new Audio(`${audio}tom-2.mp3`),
+        'd' : new Audio(`${audio}tom-3.mp3`),
+        'f' : new Audio(`${audio}tom-4.mp3`)
     }
     const sound = drumSound[key];
     if(sound) {return sound.play()};
