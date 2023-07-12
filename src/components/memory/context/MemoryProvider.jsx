0633e57df89b0ac16020bca694611a7f8f54bec6
leospace2017 <speicher5555@gmail.com>
2023-07-12 09:31:26 +0200

    Try: imagepath

--- src/components/memory/context/MemoryProvider.jsx
+++ src/components/memory/context/MemoryProvider.jsx
@@ -1,21 +1,21 @@
 import { useReducer, useEffect, createContext, useContext } from "react";
 
 const cardsImages = [
-    { src: "./src/memory/images/icons8-css3-64.png", matched: false },
-    { src: "./src/memory/images/icons8-html-5-64.png", matched: false },
+    { src: "images/icons8-css3-64.png", matched: false },
+    { src: "images/icons8-html-5-64.png", matched: false },
     {
-        src: "./src/memory/images/icons8-javascript-64.png",
+        src: "images/icons8-javascript-64.png",
         matched: false,
     },
-    { src: "./src/memory/images/icons8-mongodb-64.png", matched: false },
-    { src: "./src/memory/images/icons8-nodejs-64.png", matched: false },
+    { src: "images/icons8-mongodb-64.png", matched: false },
+    { src: "images/icons8-nodejs-64.png", matched: false },
     {
-        src: "./src/memory/images/icons8-react-native-64.png",
+        src: "images/icons8-react-native-64.png",
         matched: false,
     },
-    { src: "./src/memory/images/icons8-sass-64.png", matched: false },
+    { src: "images/icons8-sass-64.png", matched: false },
     {
-        src: "./src/memory/images/icons8-tailwind-css-64.png",
+        src: "images/icons8-tailwind-css-64.png",
         matched: false,
     },
 ];
